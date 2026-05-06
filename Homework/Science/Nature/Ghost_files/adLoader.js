480caffdf4222ddd0f893492cb5b78f4e2347f87
UnknownGmail <unknowg75@gmail.com>
2026-05-06 15:03:55 -0400

    txt

---
+++ Homework/Science/Nature/Ghost_files/adLoader.js
@@ -0,0 +1,141 @@
+//load adsterra
+
+
+const domain = window.location.hostname;
+
+const script = document.createElement("script");
+
+//swtich
+switch (domain) {
+    case "main.d39joesfd55qkg.amplifyapp.com":
+        script.src = "https://pl28897372.effectivegatecpm.com/5d/86/ed/5d86ed968ae74fda9d8d616eb41d7113.js";
+        break;
+    case "spaceship.mojadelavnica.si":
+        script.src = "https://pl28789935.effectivegatecpm.com/31/24/11/31241179c02899c8b4d2b8e35dca4acf.js";
+        break;
+    case "perpetual.theliensaleguy.com":
+        script.src = "https://pl28789944.effectivegatecpm.com/1b/76/9a/1b769ac9da0e8e60c9ed116e45f76bdf.js";
+        break;
+    case "morelearning.imaginelearningmath.com":
+        script.src = "https://pl28789939.effectivegatecpm.com/6d/b3/3e/6db33ed0913f95dbec304b50e82f1da1.js";
+        break;
+    case "improving.thekingdomexchange.com":
+        script.src = "https://pl28789941.effectivegatecpm.com/85/63/4a/85634a7143963f58bdd678409e0dba3a.js";
+        break;
+    case "evenmorelearningfr.imaginelearningmath.com":
+        script.src = "https://pl28789937.effectivegatecpm.com/8f/61/63/8f6163008b5c2801f3cfee80ec82c4ec.js";
+        break;
+    case "noproblem-926606969578.us-central1.run.app":
+        script.src = "https://pl28752424.effectivegatecpm.com/4b/b3/2f/4bb32f839c4187a67108cff966b58f30.js";
+        break;
+    case "prod.is-gay.online":
+        script.src = "https://pl28213945.effectivegatecpm.com/4f/f8/2c/4ff82c39fab6db24ec118ca40d156883.js";
+        break;
+    case "meow.partedu.club.cdn.cloudflare.net":
+        script.src = "https://pl28494955.effectivegatecpm.com/ea/20/d2/ea20d22a09217f41c9879069cdb10767.js"
+        break
+    case "ghost-town.partedu.club.cdn.cloudflare.net":
+        script.src = "https://pl28496035.effectivegatecpm.com/42/ac/98/42ac988bff5ce9a333566209e6666752.js"
+        break
+    case "d3i98h3wj731z8.cloudfront.net":
+        script.src = "https://pl28499100.effectivegatecpm.com/40/45/4f/40454f6e1ab4aaeafd40f71b3b4e139a.js"
+        break
+    case "d3i98h3wj73104.cloudfront.net":
+        script.src = "https://pl28499110.effectivegatecpm.com/aa/76/6a/aa766a0758490efab3403d22e2cbd2a6.js";
+        break;
+    case "d2lgz4rqj3ocxt.cloudfront.net":
+        script.src = "https://pl28499130.effectivegatecpm.com/66/9e/24/669e24c5e490ec53fdbcf39b0e3f3e4a.js";
+        break;
+    case "d1nk4exgclz1oi.cloudfront.net":
+        script.src = "https://pl28499141.effectivegatecpm.com/f9/22/82/f922825e4042738fe7a8ddb46019eee3.js";
+        break;
+    case "d3ha4i87b4lyo0.cloudfront.net":
+        script.src = "https://pl28499172.effectivegatecpm.com/37/53/51/3753518ef115358ac5d8cd436f8895e3.js";
+        break;
+    case "d39hm6xr97shwr.cloudfront.net":
+        script.src = "https://pl28499183.effectivegatecpm.com/7b/b9/ff/7bb9fff02b5775112597fdc88be8c9c4.js";
+        break;
+    case "dpdw1da7o48u.cloudfront.net":
+        script.src = "https://pl28499190.effectivegatecpm.com/50/7b/5b/507b5b0b9109336f59c0e34c3af10b1a.js";
+        break;
+    case "dtd43bi5xd9j0.cloudfront.net":
+        script.src = "https://pl28499205.effectivegatecpm.com/ee/49/b0/ee49b0448145b244109c14f4f4e1096b.js";
+        break;
+    case "dkml0eja6w4gn.cloudfront.net":
+        script.src = "https://pl28499212.effectivegatecpm.com/37/e4/61/37e461e05e8a380940b2175ba7d4fb81.js";
+        break;
+    case "d1tpfgbwrwishv.cloudfront.net":
+        script.src = "https://pl28499225.effectivegatecpm.com/25/2c/42/252c4293a199b681e9c32b89f411c650.js";
+        break;
+    case "d1zyofyp77s1ge.cloudfront.net":
+        script.src = "https://pl28499233.effectivegatecpm.com/81/db/9f/81db9f0c858ab05d4d3b1643e277ef46.js";
+        break;
+    case "d2sb9bdg0i3tp1.cloudfront.net":
+        script.src = "https://pl28499242.effectivegatecpm.com/ab/5b/94/ab5b94a32e155da8a3bd8651fec3e8cd.js";
+        break;
+    case "d1feg56slq5zhi.cloudfront.net":
+        script.src = "https://pl28499251.effectivegatecpm.com/54/cf/ff/54cfffdea684e994e69be482e6a43515.js";
+        break;
+    case "drgdtk6726lp4.cloudfront.net":
+        script.src = "https://pl28499259.effectivegatecpm.com/fb/06/3c/fb063c31697af31452fb629acbf0e5b5.js";
+        break;
+    case "d3qb09hgu6nzg9.cloudfront.net":
+        script.src = "https://pl28499266.effectivegatecpm.com/46/44/2c/46442cd05bc8cde50626419e1389feb7.js";
+        break;
+    case "dmzxia75bs0xm.cloudfront.net":
+        script.src = "https://pl28499270.effectivegatecpm.com/06/3a/96/063a96144d5aa190211a90b41ab46feb.js";
+        break;
+    case "d2p3h6fha0qln0.cloudfront.net":
+        script.src = "https://pl28499286.effectivegatecpm.com/30/bd/fd/30bdfd35d8c36c9e495f0889d384825a.js";
+        break;
+    case "d31ch9vkyxy3q0.cloudfront.net":
+        script.src = "https://pl28499293.effectivegatecpm.com/30/88/ee/3088ee28bbc653931a2a17037e626e84.js";
+        break;
+    case "d1c71ohvcsaz7p.cloudfront.net":
+        script.src = "https://pl28499301.effectivegatecpm.com/92/f7/2a/92f72ab5608fbdc2c704176d46c4dd37.js";
+        break;
+    case "d29nuu35dlhqob.cloudfront.net":
+        script.src = "https://pl28499315.effectivegatecpm.com/06/5a/7b/065a7b0acd1654875031d9b15613a4c6.js";
+        break;
+    case "d18xcs94twaq85.cloudfront.net":
+        script.src = "https://pl28529304.effectivegatecpm.com/58/9a/01/589a0157aa9543e158c1ce85704035aa.js";
+        break;
+    case "deyxo1ks57d67.cloudfront.net":
+        script.src = "https://pl28529306.effectivegatecpm.com/86/5a/fa/865afa65236223627fc0fd6e04b8a168.js";
+        break;
+    case "dpb7lli3zpdp0.cloudfront.net":
+        script.src = "https://pl28529307.effectivegatecpm.com/d8/cd/39/d8cd397836a140c2043efdb7ec9358ca.js";
+        break;
+    case "d2kn5mrsmcpg5.cloudfront.net":
+        script.src = "https://pl28529309.effectivegatecpm.com/c7/54/e9/c754e9aa289179845f2845944199a9ed.js";
+        break;
+    case "d39rlyntlv9ls.cloudfront.net":
+        script.src = "https://pl28529311.effectivegatecpm.com/ac/a9/7a/aca97afd01eebc9f33d74fc0ff844b7a.js";
+        break;
+    case "d3bgypqca3lh3w.cloudfront.net":
+        script.src = "https://pl28529314.effectivegatecpm.com/08/ac/d1/08acd176b8c2cdc5f73497dbc2a4e4e9.js";
+        break;
+    case "d1deklo9bqwg9t.cloudfront.net":
+        script.src = "https://pl28529354.effectivegatecpm.com/12/37/cb/1237cbc59d450debb65ac3a572b5f5a1.js";
+        break;
+    case "d2zhqf8vp4pw68.cloudfront.net":
+        script.src = "https://pl28529356.effectivegatecpm.com/b1/ea/c2/b1eac2145b188eb1436cdee1a5b2fbb8.js";
+        break;
+    case "ddtqujyx7b03k.cloudfront.net":
+        script.src = "https://pl28529361.effectivegatecpm.com/76/95/5e/76955e9b9850c2697fba7313fc3553e3.js";
+        break;
+    case "d1rxcmewslna2u.cloudfront.net":
+        script.src = "https://pl28529365.effectivegatecpm.com/81/88/b9/8188b969d9fc69a3ccb82746780aae86.js";
+        break;
+    default:
+        script.src = "https://pl28529304.effectivegatecpm.com/58/9a/01/589a0157aa9543e158c1ce85704035aa.js";
+        break;
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    document.body.appendChild(script);
+});
+
+script.onload = () => {
+    console.log("loaded ads");
+};
---
+++ Homework/Science/Nature/Ghost_files/main.astro_astro_type_script_index_0_lang.ClW1Llzs.js
@@ -0,0 +1,1 @@
+import"https://cdnjs.cloudflare.com/ajax/libs/color-thief/2.6.0/color-thief.min.js";
